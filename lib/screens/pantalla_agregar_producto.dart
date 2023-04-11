58ed9a821be7b872fe118dde2d2a56fd0ef46a19
Juan Felipe <juanfelipecortescastrillon@gmail.com>
2023-04-10 20:53:54 -0500

    Se agrega boton que lleva a otra pantalla para agregar algo a la lista

---
+++ lib/screens/pantalla_agregar_producto.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class PantallaAgregarProducto extends StatefulWidget {
+  const PantallaAgregarProducto({super.key});
+
+  @override
+  State<PantallaAgregarProducto> createState() =>
+      _PantallaAgregarProductoState();
+}
+
+class _PantallaAgregarProductoState extends State<PantallaAgregarProducto> {
+  final _controladorTexto = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _controladorTexto,
+              decoration: const InputDecoration(labelText: 'Nuevo Elemento'),
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop(_controladorTexto.text);
+                },
+                child: const Text('Agregar'))
+          ],
+        ),
+      ),
+    );
+  }
+}
