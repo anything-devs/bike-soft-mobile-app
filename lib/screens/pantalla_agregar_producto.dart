ee81b3ec5de87362bff8d0d111111994e5f9b5d7
Juan Felipe <juanfelipecortescastrillon@gmail.com>
2023-04-10 22:24:55 -0500

    Se agrega la funcionalidad para que se pueda pueda devolverse con los botones del sistema

--- lib/screens/pantalla_agregar_producto.dart
+++ lib/screens/pantalla_agregar_producto.dart
@@ -12,25 +12,31 @@ class _PantallaAgregarProductoState extends State<PantallaAgregarProducto> {
   final _controladorTexto = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextField(
-              controller: _controladorTexto,
-              decoration: const InputDecoration(labelText: 'Nuevo Elemento'),
-            ),
-            const SizedBox(
-              height: 16.0,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop(_controladorTexto.text);
-                },
-                child: const Text('Agregar'))
-          ],
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.of(context).pop('');
+        return true;
+      },
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                controller: _controladorTexto,
+                decoration: const InputDecoration(labelText: 'Nuevo Elemento'),
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(_controladorTexto.text);
+                  },
+                  child: const Text('Agregar'))
+            ],
+          ),
         ),
       ),
     );
