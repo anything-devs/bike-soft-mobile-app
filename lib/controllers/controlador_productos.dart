029ff632fb811aa210915002b698ebcd3bd5293a
Juan Felipe <juanfelipecortescastrillon@gmail.com>
2023-04-12 09:53:07 -0500

    Se agrega el filtro de Z-A y lista en este orden

--- lib/controllers/controlador_productos.dart
+++ lib/controllers/controlador_productos.dart
@@ -4,15 +4,15 @@ import 'package:bike_soft_mobile_app/models/producto.dart';
 import 'package:http/http.dart' as http;
 
 class ControladorProductos {
-  Future<List<Producto>> getProductos() async {
-    var url = Uri.parse("http://10.0.2.2:8080/productos");
+  Future<List<Producto>> getProductos(String filtro) async {
+    var url = Uri.parse("http://10.0.2.2:8080/$filtro");
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
       final List<dynamic> data = jsonDecode(response.body);
       return data.map((json) => Producto.fromJson(json)).toList();
     } else {
-      throw Exception("Error al obtener productos");
+      throw Exception("Error al&parseTime=true obtener productos");
     }
   }
 
